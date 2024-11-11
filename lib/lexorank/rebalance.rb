b99bc486ce4bc55f16c5859d8eb55537e4b2c179
Will Fleming <will@flemi.ng>
2024-11-11 10:05:30 -0500

    refactor rebalancing, addressing bugs & improving benchmarking
    Banged my head against a number of things here, and this still isn't
    exactly what I want but I think it's usable or at least moves the ball
    forward a bit.
    
    I've been comparing the implementation here to django-lexorank &
    lexorank-ts, which is helpful in some ways but all the implementations
    all seem to conceptualize lexorank a bit differently so their
    increment/rebalance approaches aren't trivially translateable either.
    
    I think to really do this fully correctly the implementation here would
    probably have to grow to consider buckets and things, which I've been
    kind of trying to avoid.
    
    A few stumbling blocks I hit:
    
    - carry over in addition/incrementing. The basic idea here seems to be
      treating each rank as a base-n number. The django-lexorank
      implementation includes a carry-over at the end to prepend new
      "digits", but I think this is buggy since lexorank sorting doesn't
      work like normal numerical sorting. E.g. if you increment a key like
      `gT` a large amount and it carries over, it might end up as like
      `17R`, which is more "digits" but sorts earlier by string semantics.
    
      What I ended up with is a check to treat any leftover carry during
      increment as a bug. I think the approach to choosing the length of
      keys and the increment between them ensures there shouldn't in
      practice be any leftover during incrementing.
    - end of keyspace being inclusive or exclusive. In other implementations
      I've been reading, 'z' or 'zzzz' or whatever would be a valid key, but
      this implementation is treating 'z' as an unreachable "infinity" in
      the keyspace, so I adjusted the step calculation to adjust for that.
      FWIW i think in practice the implementation here would handle keys
      above 'z' fine, so this might be an artificial limitation that's not
      really needed? I don't want to get sidetracked by rewriting
      fundamental behavior here, though.
    - batches. By default batches in Rails ignore any existing `order` on
      your query and use primary key, I'd forgotten that, that created some
      bad bugs with things ending up in the wrong order after rebalancing.
      I'm also not 100% convinced this algorithm couldn't "leapfrog"
      existing records in a later batch during the key incrementing, so I
      moved from updating each batch as it's fetched to first calculating
      the entire set of new ranks in memory and then doing the upserts in
      batches. I'm dissatisfied with this since it has some performance
      risks, but in my benchmarking (see discussion below) I think it's
      tolerable for realistic use. I think the "proper" way of doing it
      means adding buckets, which, again, I'm trying not to do.
    
    \## Benchmarking
    
    The benchmark script here is both a perf test tool & a bit of a fuzz
    tester on correctness since a unit test rebalancing large tables and
    ensuring order is correct is probably not something you'd want running
    every time you run the test suite.
    
    Perf here feels pretty acceptable. Time to rebalance a 10k item table is
    ~300-400ms on my laptop. A 100k item table is ~3s. Realistic usage will
    probably be a bit worse since this is an in-memory sqlite database and
    latency with postgres/mysql is likely to be the bottleneck in real
    usage, but for reasonably sized tables I think it's fast enough that the
    duration of the operation (and the lock) is unlikely to be a big issue.
    
    Memory usage is of course a bit higher since I'm building the full set
    of updates in memory, but the fact that we only select id and rank helps
    there. Even with 100k items, memory usage of the benchmark script was
    only 50-something MB, so I think it's acceptable.

--- lib/lexorank/rebalance.rb
+++ lib/lexorank/rebalance.rb
@@ -3,7 +3,7 @@
 class Lexorank::Rebalance
   include Lexorank
 
-  attr_reader :ranking, :group_vals, :batch_size
+  attr_reader :ranking, :group_vals, :batch_size, :collection, :record_count
 
   def initialize(ranking:, group_vals:, batch_size: 1_000)
     @ranking = ranking
@@ -13,6 +13,9 @@ class Lexorank::Rebalance
     if ranking.group_by && (!group_vals || ranking.group_by.any? { |k| !group_vals.include?(k) })
       raise ArgumentError.new("This table's ranks are grouped by #{ranking.group_by}, not all keys were present")
     end
+
+    @collection = ranking.record_class.where(group_vals).ranked.select(ranking.record_class.primary_key, ranking.field)
+    @record_count = collection.unscope(:select).count
   end
 
   def execute
@@ -20,23 +23,104 @@ class Lexorank::Rebalance
       # need a record instance for advisory lock name
       first_instance = ranking.record_class.where(group_vals).first
 
-      last_rank = nil
-      scope = ranking.record_class.where(group_vals).ranked.select(ranking.record_class.primary_key, ranking.field)
+      last_rank = MIN_CHAR * rank_length
+      all_updates = []
+
       ranking.with_lock_if_enabled(first_instance) do
-        scope.in_batches(of: batch_size) do |batch|
-          last_rank = process_batch(batch, last_rank)
+        process_batches(collection) do |batch|
+          batch_updates, last_rank = build_batch_updates(batch, last_rank)
+          all_updates += batch_updates
         end
+
+        perform_updates(all_updates)
       end
     end
   end
 
-  def process_batch(batch, last_rank)
-    updates = []
-    batch.each do |record|
-      last_rank = value_between(last_rank, nil)
-      updates << Hash[record.class.primary_key, record.send(record.class.primary_key), ranking.field, last_rank]
+  def build_batch_updates(batch, last_rank)
+    updates = batch.map do |record|
+      last_rank = increment_rank(last_rank)
+      Hash[record.class.primary_key, record.send(record.class.primary_key), ranking.field, last_rank]
     end
-    ranking.record_class.upsert_all(updates)
-    last_rank
+    [updates, last_rank]
+  end
+
+  def perform_updates(all_updates)
+    all_updates.each_slice(batch_size) do |batch_updates|
+      ranking.record_class.upsert_all(batch_updates)
+    end
+  end
+
+  private
+
+  def symbol_count
+    @symbol_count ||= (MAX_CHAR.ord - MIN_CHAR.ord) + 1
+  end
+
+  def rank_length
+    @rank_length ||=
+      begin
+        length_required_to_place_all = Math.log(record_count, symbol_count).ceil
+        length_required_to_place_all * 2
+      end
+  end
+
+  def rank_step
+    @rank_step ||=
+      begin
+        # subtract highest order of magnitude because high end of symbols is exclusive (no rank >= z)
+        possible_values = (symbol_count**rank_length) - (symbol_count**(rank_length-1))
+        # want the spread to include space at beginning and end
+        possible_values / (record_count + 2)
+      end
+  end
+
+  def char_to_offset_ord(char)
+    char.ord - MIN_CHAR.ord
+  end
+
+  def char_from_offset_ord(rel_ord)
+    (MIN_CHAR.ord + rel_ord).chr
+  end
+
+  def increment_rank(rank)
+    step = rank_step
+    chars = rank.chars
+    next_chars = chars.reverse_each.map do |c|
+      if step.zero?
+        c
+      else
+        tot = char_to_offset_ord(c) + step
+        step = tot / symbol_count
+        char_from_offset_ord(tot % symbol_count)
+      end
+    end.compact.reverse_each.to_a
+
+    next_rank = next_chars.join('')
+
+    if step > 0 # this is equivalent to carrying leftover, shouldn't happen since we've constructed the key space to all be the same length (number of "digits")
+      raise InvalidRankError, 'looks like a bug: no carry over should happen during rebalance'
+    elsif next_rank < rank
+      raise InvalidRankError, 'looks like a bug: next rank ended up < last rank'
+    elsif next_rank >= MAX_CHAR
+      raise InvalidRankError, 'looks like a bug: no rank should be >= z'
+    end
+
+    next_rank
+  end
+
+  # ActiveRecord `in_batches` requires the cursor field (which would be `rank`) to be unique, but
+  # while this gem encourages making rank unique it does not enforce it, and it's not unique indexed
+  # in test models, so we do our own batching
+  def process_batches(collection)
+    pk_field = ranking.record_class.primary_key
+    batch = collection.limit(batch_size).load
+
+    while batch.any?
+      yield batch
+      last_rank = batch.last.send(ranking.field)
+      ignore_ids = batch.reverse_each.take_while { |r| r.send(ranking.field) == last_rank }.map { |r| r.send(pk_field) }
+      batch = collection.where("rank >= ?", last_rank).where.not(pk_field => ignore_ids).limit(batch_size).load
+    end
   end
 end
